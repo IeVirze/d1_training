26f1a36ba0dd312b4e8c04c9b477c8fb2aa00f76
IeVirze <136879608+IeVirze@users.noreply.github.com>
2024-05-16 13:27:14 +0300

    js un css

--- 1200/project/script.js
+++ 1200/project/script.js
@@ -4,6 +4,9 @@
 function getValue(event) {
     const num = event.target.getAttribute("value")
 
+    //Pārbauda vai konsolē tiek izvadīta pogas vērtība pareizi
+    console.log(num)
+
     document.getElementById("display").innerHTML += num;
 }
 
@@ -15,5 +18,5 @@ function clear() {
 
 //funkcija result(), kas parāda aprēķināto rezultātu
 function result() {
-    
+
 }
