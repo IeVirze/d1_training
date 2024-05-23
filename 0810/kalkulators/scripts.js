1de4b01b429e222a8b18cba92688247cb68b76ed
IeVirze <136879608+IeVirze@users.noreply.github.com>
2024-05-23 12:32:25 +0300

    .

--- 0810/kalkulators/scripts.js
+++ 0810/kalkulators/scripts.js
@@ -2,16 +2,27 @@
 
 function takeValue(event) {
     const num = event.target.getAttribute("value")
-
+    let input = document.getElementById('display')
+    let text = input.innerHTML;
     //console.log(num)
-
     document.getElementById('display').innerHTML += num;
+
+    //ja display ievadītā teksta garums ir lielāks par 15 rakstzīmēm izpildīt sekojošo
+    if (text.length > 15 ) {
+        clearInput();
+    } 
 }
 
-function clear(){
 
+//C pogas funkcija - notīrīt visu
+function clearInput() {
+    display.innerHTML = "";
 }
 
+//= funkcija parādīt rezultātu
 function showResult() {
-
+    //mainīgais, kas paņem displeja innerHTML vērtību un pēc tās aprēķina rezultātu
+    //eval() šajā mācību projektā ir pieņemams, bet reālos projektos no tā vajag izvairīties
+    const result = eval(document.getElementById('display').innerHTML);
+    document.getElementById('display').innerHTML = result;
 }
--- 1200/project/script.js
+++ 1200/project/script.js
@@ -3,20 +3,25 @@
 //funkcija getValue(event)
 function getValue(event) {
     const num = event.target.getAttribute("value")
-
     //Pārbauda vai konsolē tiek izvadīta pogas vērtība pareizi
     console.log(num)
-
-    document.getElementById("display").innerHTML += num;
+    document.getElementById("display").innerHTML += num; 
 }
 
 //funkcija clear(), kas notīra visas vērtības
-function clear() {
+function clearInput() {
+    console.log("Cipari notīrīti!")
 
+    //Lai nodzēstu displeja vērtību to aizstājam ar tukšu teksta string
+    document.getElementById("display").innerHTML = "";
 }
 
 
 //funkcija result(), kas parāda aprēķināto rezultātu
 function result() {
-
+    console.log("Rezultāts iegūts")
+    //mainīgais, kurš satur rezultātu
+    // Iebūvētā JS funkcija - eval()
+    let result = eval(document.getElementById("display").innerHTML)
+    document.getElementById("display").innerHTML = result;
 }
